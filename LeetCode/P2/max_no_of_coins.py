6a6653ce3fd351fe79fb4bc379181c00507d3b21
MagMc007 <mergashasho7@gmail.com>
2026-02-20 14:31:57 +0300

    1561

--- LeetCode/P2/max_no_of_coins.py
+++ LeetCode/P2/max_no_of_coins.py
@@ -1,7 +1,22 @@
 # https://leetcode.com/problems/maximum-number-of-coins-you-can-get/description/
 # 1561. Maximum Number of Coins You Can Get
 class Solution:
-    def maxCoins(self, piles: List[int]) -> int:
+    def maxCoins(self, piles: list[int]) -> int:
         piles.sort(reverse=True)
 
-        pass
+        n = len(piles)
+        
+        # how many am i taking ??
+        times = n // 3
+        output = 0
+
+        # to maximize you coins, bob needs to take the lowest 
+        # possible coins
+        # smalles n coins belong to bob
+        for i in range(1, times * 2, 2):
+            output += piles[i]
+        
+        return output
+ 
+soln =  Solution()
+print(soln.maxCoins([9,8,7,6,5,1,2,3,4]))
