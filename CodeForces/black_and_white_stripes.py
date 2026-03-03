aa454b533b7bd9f27f7292c5776e18954dce842a
MagMc007 <mergashasho7@gmail.com>
2026-03-03 14:23:48 +0300

    7

--- CodeForces/black_and_white_stripes.py
+++ CodeForces/black_and_white_stripes.py
@@ -1,2 +1,30 @@
 # https://codeforces.com/problemset/problem/1690/D
 # D. Black and White Stripe
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    block = input()
+
+    # fixed sliding window of size k
+    paint = 0
+    for i in range(k):
+        if block[i] == 'W':
+            paint += 1
+
+    min_ = paint  # record the minimum paint needed
+
+    left = 0
+    for right in range(k, n):
+        # add from the right
+        if block[right] == 'W':
+            paint += 1
+        
+        # move the left
+        if block[left] == 'W':
+            paint -= 1
+        left += 1
+
+        min_ = min(min_, paint)
+
+    print(min_)
---
+++ CodeForces/flip_the_bits.py
@@ -0,0 +1,10 @@
+# https://codeforces.com/problemset/problem/1504/B
+# B. Flip the Bits
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    
