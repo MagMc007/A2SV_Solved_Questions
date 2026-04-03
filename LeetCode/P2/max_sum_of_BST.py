681edfe0ede9b1eafe2fc007959f1646603d0ae5
MagMc007 <mergashasho7@gmail.com>
2026-04-03 19:57:01 +0300

    57

--- LeetCode/P2/max_sum_of_BST.py
+++ LeetCode/P2/max_sum_of_BST.py
@@ -6,6 +6,28 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
 class Solution:
     def maxSumBST(self, root: Optional[TreeNode]) -> int:
-        ...
+        self.max_total_sum = 0
+        
+        def postorder(node):
+            if not node:
+                return True, float('inf'), float('-inf'), 0
+            
+            # chekc children (using call stack)
+            left_is_bst, left_min, left_max, left_sum = postorder(node.left)
+            right_is_bst, right_min, right_max, right_sum = postorder(node.right)
+            
+            # check current node is BST
+            if left_is_bst and right_is_bst and left_max < node.val < right_min:
+                current_sum = node.val + left_sum + right_sum
+                self.max_total_sum = max(self.max_total_sum, current_sum)
+                
+                # pass on the min from the right side, max of left 
+                return True, min(node.val, left_min), max(node.val, right_max), current_sum
+            
+            return False, 0, 0, 0 # start fresh on the sum
+
+        postorder(root)
+        return self.max_total_sum
