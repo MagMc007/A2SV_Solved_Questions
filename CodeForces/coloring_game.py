8db725bef9131050ed44079718f4f6b6bd05e1f1
MagMc007 <mergashasho7@gmail.com>
2026-04-06 22:33:17 +0300

    60

--- CodeForces/coloring_game.py
+++ CodeForces/coloring_game.py
@@ -6,4 +6,33 @@ for _ in range(t):
     n = int(input())
     arr = list(map(int, input().split()))
 
-    
+    # arr is provide in sorted order
+
+    ans = 0
+    max_ = arr[-1]
+
+    # to win
+    # 1. bob recolors Alice's largest, so alice needs: a[i] + a[j] > a[k]
+    # 2. bob colors the largest uncolored (max_), Alice needs: a[i] + a[j] + a[k] > max_
+    # so a[i] + a[j] > max_- a[k]
+
+    for k in range(2, n):
+        need = max(arr[k], max_ - arr[k])
+
+        i = 0 # smalles available
+        j = k - 1  # largest in the current range
+
+        while i < j:
+            # go back and look for all combinations
+            if arr[i] + arr[j] > need:
+                ans += j - i
+
+                j -= 1
+            else:
+                i += 1
+
+    print(ans)
+
+
+
+
--- LeetCode/P2/remove_invalid_parenthesis.py
+++ LeetCode/P2/remove_invalid_parenthesis.py
@@ -2,4 +2,41 @@
 # 301. Remove Invalid Parentheses
 class Solution:
     def removeInvalidParentheses(self, s: str) -> List[str]:
-        ...
+        # Step 1: count invalid parentheses
+        l = r = 0
+        for c in s:
+            if c == '(':
+                l += 1
+            elif c == ')':
+                if l > 0:
+                    l -= 1
+                else:
+                    r += 1
+
+        res = set()
+
+        def dfs(i, l, r, open_cnt, path):
+            if i == len(s):
+                if l == 0 and r == 0 and open_cnt == 0:
+                    res.add(path)
+                return
+
+            c = s[i]
+
+            # REMOVE
+            if c == '(' and l > 0:
+                dfs(i+1, l-1, r, open_cnt, path)
+            elif c == ')' and r > 0:
+                dfs(i+1, l, r-1, open_cnt, path)
+
+            # KEEP
+            if c == '(':
+                dfs(i+1, l, r, open_cnt+1, path + c)
+            elif c == ')':
+                if open_cnt > 0:
+                    dfs(i+1, l, r, open_cnt-1, path + c)
+            else:
+                dfs(i+1, l, r, open_cnt, path + c)
+
+        dfs(0, l, r, 0, "")
+        return list(res)
