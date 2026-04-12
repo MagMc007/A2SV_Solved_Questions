b8deae7422c572efdf9d3800d04f8ee11126a73f
MagMc007 <mergashasho7@gmail.com>
2026-04-12 21:29:15 +0300

    78

--- CodeForces/longest_k_good_segments.py
+++ CodeForces/longest_k_good_segments.py
@@ -1,2 +1,31 @@
 # https://codeforces.com/problemset/problem/616/D
 # D. Longest k-Good Segment
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+
+cnt = {}
+distinct = 0
+
+l = 0
+best_l = 0
+best_r = 0
+max_len = 0
+
+for r in range(n):
+    if a[r] not in cnt or cnt[a[r]] == 0:
+        distinct += 1
+        cnt[a[r]] = 0
+    cnt[a[r]] += 1
+
+    while distinct > k:
+        cnt[a[l]] -= 1
+        if cnt[a[l]] == 0:
+            distinct -= 1
+        l += 1
+
+    if r - l + 1 > max_len:
+        max_len = r - l + 1
+        best_l = l
+        best_r = r
+
+print(best_l + 1, best_r + 1)
