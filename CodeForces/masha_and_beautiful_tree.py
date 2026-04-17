6188cce291b9027fa681ca0bf9a6350d72fc117c
MagMc007 <mergashasho7@gmail.com>
2026-04-17 19:51:24 +0300

    85

--- CodeForces/masha_and_beautiful_tree.py
+++ CodeForces/masha_and_beautiful_tree.py
@@ -1,55 +1,60 @@
 # https://codeforces.com/problemset/problem/1741/D
 # D. Masha and a Beautiful Tree
 import sys
-# print(sys.getrecursionlimit())
-sys.setrecursionlimit(10000)
-t = int(input())
+
+sys.setrecursionlimit(2000)
+
+input_data = sys.stdin.read().split()
+idx = 0
+
+t = int(input_data[idx])
+idx += 1
+
+results = []
 
 for _ in range(t):
-    m = int(input())
-    p = list(map(int, input().split()))
-
-    # merge sort the array
-    # should not go to the end(last element instead return when we have
-    # 2 elements) guerantees the permutation is power of 2
-    cnt = 0
-
-    def mergeArrOf2(eleft, elright):
-        global cnt
-
-        if eleft > elright:
-            cnt += 1
-            return [elright, eleft]    
-        
-        return [eleft, elright]
-
-    def checker(leftArr, rightArr):
-        global cnt
-
-        # can only compare the first elements
-        if leftArr[0] > rightArr[0]:
-            cnt += 1
-            return rightArr + leftArr
-        
-        return False
-
-    def mergeSort(left, right, arr):
-        # base case
-        # lenght must be 2
-        if right - left + 1 == 2:
-            return mergeArrOf2(arr[left], arr[right])
-        
-        mid = left + (right - left) // 2
-
-        left = mergeSort(left, mid, arr)
-        right = mergeSort(mid+1, right, arr)
-
-        if left and right:
-            return checker(left, right)
-        return False
+    m = int(input_data[idx])
+    idx += 1
     
-    ans = mergeSort(0, m-1, p)
-    if ans:
-        print(cnt)
+    p = list(map(int, input_data[idx: idx + m]))
+    idx += m
+    
+    cnt = 0
+    possible = True
+
+    def divide_and_conquer(l, r):
+        global cnt, possible
+        
+        length = r - l
+        
+        if length == 1:
+            return p[l]
+        
+        mid = (l + r) // 2
+        
+        left_min = divide_and_conquer(l, mid)
+        right_min = divide_and_conquer(mid, r)
+        
+        if not possible:
+            return -1
+        
+        half_size = length // 2
+        
+        if abs(left_min - right_min) != half_size:
+            possible = False
+            return -1
+        
+        if left_min > right_min:
+            cnt += 1
+            return right_min
+        else:
+            return left_min
+
+    divide_and_conquer(0, m)
+
+    if possible:
+        results.append(str(cnt))
     else:
-        print(-1)
+        results.append("-1")
+
+sys.stdout.write("\n".join(results) + "\n")
